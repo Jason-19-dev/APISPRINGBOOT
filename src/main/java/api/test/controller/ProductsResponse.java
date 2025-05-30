bd07455b39b2128a0ef8ccd423045c23fdb92362
Jason Nuñez <jnunez@bgeneral.com>
2025-05-30 14:52:02 -0500

    commit test

--- src/main/java/api/test/controller/ProductsResponse.java
+++ src/main/java/api/test/controller/ProductsResponse.java
@@ -4,19 +4,17 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
-
 @RestController
-@RestMapping("/products")
+@RequestMapping("/products")
 public class ProductsResponse {
  
-	// private List<Product> products = new ArrayList<>();
-	@GetMapping("/{productId}")
-	public void getProduct() {	
+    // private List<Product> products = new ArrayList<>();
+    @GetMapping("/{productId}")
+    public void getProduct() {
 		
 		return null;
 	}
