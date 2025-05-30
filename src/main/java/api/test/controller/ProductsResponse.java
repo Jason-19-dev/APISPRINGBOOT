08311ab3219382e7e34866f9ba14cee170f34f63
Jason Nuñez <jnunez@bgeneral.com>
2025-05-30 14:27:50 -0500

    commit test

--- src/main/java/api/test/controller/ProductsResponse.java
+++ src/main/java/api/test/controller/ProductsResponse.java
@@ -1,10 +1,14 @@
-// packeage api.test.controller;
+packeage api.test.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
+import api.test.model.Product;
+
 
 @RestController
 @RestMapping("/products")
@@ -12,13 +16,13 @@ public class ProductsResponse {
  
 	private List<Product> products = new ArrayList<>();
 	@GetMapping("/{productId}")
-	public User getProduct(@PathVariable Long productId) {
+	public Product getProduct() {
 		
 		return null;
 	}
 
 	@DeleteMapping("/{productId}")
-	public void deleteProduct(@PathVariable Long productId) {
+	public void deleteProduct() {
 		return null;
 	}
 }
