429005366ee4e7d4fc31c7e3562a3e3b6c5fbb6d
Jason Nuñez <jnunez@bgeneral.com>
2025-05-30 14:48:24 -0500

    test

--- src/main/java/api/test/controller/ProductsResponse.java
+++ src/main/java/api/test/controller/ProductsResponse.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
